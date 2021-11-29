7931986df73684092b590dc81a91130c2de86b47
Kgk01040 <95196088+Kgk01040@users.noreply.github.com>
2021-11-28 19:26:58 -0600

    Add files via upload

---
+++ Funciones.js
@@ -0,0 +1,101 @@
+function H_Camisas() {
+    document.getElementById("C_img_1").src="Camisa_1.jpg"
+    document.getElementById("C_P_1").textContent="Precio: 8.000 colones";
+    document.getElementById("C_img_2").src="Camisa_2.jpg"
+    document.getElementById("C_P_2").textContent="Precio: 10,500 colones";
+    document.getElementById("C_img_3").src="Camisa_3.jpg"
+    document.getElementById("C_P_3").textContent="Precio: 5.400 colones";
+    document.getElementById("C_img_4").src="/Camisa_4.webp"
+    document.getElementById("C_P_4").textContent="Precio: 6.750 colones";
+    document.getElementById("C_img_5").src="Camisa_5.jpg"
+    document.getElementById("C_P_5").textContent="Precio: 9.790 colones";
+    document.getElementById("C_img_6").src="Camisa_6.jpg"
+    document.getElementById("C_P_6").textContent="Precio: 10.650 colones";
+    document.getElementById("C_img_7").src="Camisa_7.jpg"
+    document.getElementById("C_P_7").textContent="Precio: 12.000 colones";
+}
+
+function  H_Sudaderas() {
+    document.getElementById("C_img_1").src="Sudadera_1.jpg";
+    document.getElementById("C_P_1").textContent="Precio: 10.500 colones"; 
+    document.getElementById("C_img_2").src="Sudadera_2.jpg";
+    document.getElementById("C_P_2").textContent="Precio: 14.000 colones";
+    document.getElementById("C_img_3").src="Sudadera_3.jpg";
+    document.getElementById("C_P_3").textContent="Precio: 16.700 colones";
+    document.getElementById("C_img_4").src="Sudadera_4.png";
+    document.getElementById("C_P_4").textContent="Precio: 11.200 colones";
+    document.getElementById("C_img_5").src="Sudadera_5.jpg";
+    document.getElementById("C_P_5").textContent="Precio: 20.995 colones";
+    document.getElementById("C_img_6").src="Sudadera_6.jpg";
+    document.getElementById("C_P_6").textContent="Precio: 12.800 colones";
+    document.getElementById("C_img_7").src="Sudadera_7.jpg";
+    document.getElementById("C_P_6").textContent="Precio: 18.750 colones";
+}
+
+function H_Pantalones() {
+    document.getElementById("C_img_1").src="Pantalon_1.jpg"
+    document.getElementById("C_P_1").textContent="Precio: 14.000 colones";
+    document.getElementById("C_img_2").src="Pantalon_2.jpg"
+    document.getElementById("C_P_2").textContent="Precio: 12.990 colones";
+    document.getElementById("C_img_3").src="Pantalon_3.jpg"
+    document.getElementById("C_P_3").textContent="Precio: 18.100 colones";
+    document.getElementById("C_img_4").src="Pantalon_4.jpg"
+    document.getElementById("C_P_4").textContent="Precio: 22.375 colones";
+    document.getElementById("C_img_5").src="Pantalon_5.jpg"
+    document.getElementById("C_P_5").textContent="Precio: 10.800 colones";
+    document.getElementById("C_img_6").src="Pantalon_6.jpg"
+    document.getElementById("C_P_6").textContent="Precio: 14.650 colones";
+    document.getElementById("C_img_7").src="Pantalon_7.jpg"
+    document.getElementById("C_P_7").textContent="Precio: 19.900 colones";
+}
+
+function M_Blusas() {
+    document.getElementById("B_img_1").src="Blusa_1.jpg"
+    document.getElementById("B_P_1").textContent="Precio: 3.500 colones"
+    document.getElementById("B_img_2").src="Blusa_2.jpg"
+    document.getElementById("B_P_2").textContent="Precio: 5.600 colones"
+    document.getElementById("B_img_3").src="Blusa_3.jpg"
+    document.getElementById("B_P_3").textContent="Precio: 8.890 colones"
+    document.getElementById("B_img_4").src="Blusa_4.jpg"
+    document.getElementById("B_P_4").textContent="Precio: 6.500 colones"
+    document.getElementById("B_img_5").src="Blusa_5.jpg"
+    document.getElementById("B_P_5").textContent="Precio: 5.995 colones"
+    document.getElementById("B_img_6").src="Blusa_6.jpg"
+    document.getElementById("B_P_6").textContent="Precio: 10.900 colones"
+    document.getElementById("B_img_7").src="Blusa_7.webp"
+    document.getElementById("B_P_7").textContent="Precio: 11.000 colones"
+}
+
+function M_Pantalones() {
+    document.getElementById("B_img_1").src="Pantalon_B_1.jpg"
+    document.getElementById("B_P_1").textContent="Precio: 12.000 colones"
+    document.getElementById("B_img_2").src="Pantalon_B_2.jpg"
+    document.getElementById("B_P_2").textContent="Precio: 11.000 colones"
+    document.getElementById("B_img_3").src="Pantalon_B_3.jpg"
+    document.getElementById("B_P_3").textContent="Precio: 15.000 colones"
+    document.getElementById("B_img_4").src="Pantalon_B_4.jpg"
+    document.getElementById("B_P_4").textContent="Precio: 13.450 colones"
+    document.getElementById("B_img_5").src="Pantalon_B_5.jpg"
+    document.getElementById("B_P_5").textContent="Precio: 10.900 colones"
+    document.getElementById("B_img_6").src="Pantalon_B_6.jpg"
+    document.getElementById("B_P_6").textContent="Precio: 9.600 colones"
+    document.getElementById("B_img_7").src="Pantalon_B_7.jpg"
+    document.getElementById("B_P_7").textContent="Precio: 17.900 colones"
+}
+
+function M_Vestidos() {
+    document.getElementById("B_img_1").src="Vestido_1.jpg"
+    document.getElementById("B_P_1").textContent="Precio: 21.750 colones"
+    document.getElementById("B_img_2").src="Vestido_2.jpg"
+    document.getElementById("B_P_2").textContent="Precio: 25.890 colones"
+    document.getElementById("B_img_3").src="Vestido_3.jpg"
+    document.getElementById("B_P_3").textContent="Precio: 26.900 colones"
+    document.getElementById("B_img_4").src="Vestido_4.jpg"
+    document.getElementById("B_P_4").textContent="Precio: 30.900 colones"
+    document.getElementById("B_img_5").src="Vestido_5.webp"
+    document.getElementById("B_P_5").textContent="Precio: 14.200 colones"
+    document.getElementById("B_img_6").src="Vestido_6.jpg"
+    document.getElementById("B_P_6").textContent="Precio: 16.995 colones"
+    document.getElementById("B_img_7").src="Vestido_7.jpg"
+    document.getElementById("B_P_7").textContent="Precio: 19.895 colones"
+}
